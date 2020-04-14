f0e4a2188b03e7ac5c5f315324388eeb2518acc3
Gardnom <43353610+Gardnom@users.noreply.github.com>
2020-04-14 18:19:41 +0200

    First draft
    version 1.0

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const app = express()
+const port = 80
+
+
+
+const logger = (req,res,next)=>{
+    let ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+    console.log(`Incoming request from ip: ${ip}`)
+    next()
+}
+app.use(logger)
+app.set('view engine','ejs')
+
+app.get('/',(req,res)=>{
+    res.end('Welcome to index page!')
+
+})
+app.listen(port,()=>{
+    console.log(`Server running on port: ${port}`)
+})
+
